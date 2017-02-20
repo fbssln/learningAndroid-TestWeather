e92cb50f46958b7fa94259d0368ef68c69222d8b
fbssln <fbssln@163.com>
2017-02-20 14:12:34 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/fbssln/helloworld/testweather/db/City.java
@@ -0,0 +1,46 @@
+package com.fbssln.helloworld.testweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by dev7-fub on 2017/2/20.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
---
+++ app/src/main/java/com/fbssln/helloworld/testweather/db/County.java
@@ -0,0 +1,46 @@
+package com.fbssln.helloworld.testweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by dev7-fub on 2017/2/20.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
